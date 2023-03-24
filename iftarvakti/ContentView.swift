275adeae2fa0c6a786973fb5ad76edb8ccb2d3b8
Yusuf Usta <yusufusta@mobilevictory.com>
2023-03-24 14:11:55 +0300

    Initial Commit

---
+++ iftarvakti/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iftarvakti
+//
+//  Created by Yusuf Usta on 24.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iftarvakti/iftarvaktiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iftarvaktiApp.swift
+//  iftarvakti
+//
+//  Created by Yusuf Usta on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iftarvaktiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
